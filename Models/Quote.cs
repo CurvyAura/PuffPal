59d0c6813e84ed82aedf37f8e42d921948f2c604
Brett <CurvyAura@gmail.com>
2025-04-17 01:05:51 -0600

    add test page

---
+++ Models/Quote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuffPal.Models
+{
+    public class Quote : Motivation
+    {
+        public int QuoteID { get; set; }
+
+        public override void DisplayMessage()
+        {
+            Console.WriteLine($"Quote: {TextBody}"); //Temp
+        }
+    }
+}
