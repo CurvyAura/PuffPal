be62635682c0d6fc8b2ecf82abe35513cecb12ec
ConnorYasin <ConnorYasin@gmail.com>
2025-04-21 17:25:28 -0600

    Full HomePage Logic(no database) and user changed to userinfo

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PuffPal.Models
 {
-    public class User
+    public class UserInfo
     {
         public int UserId { get; set; }
         public string Name { get; set; }
