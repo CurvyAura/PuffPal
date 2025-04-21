a7a521646cdec802a75b865ade3639b4fb95ee7d
ConnorYasin <ConnorYasin@gmail.com>
2025-04-21 16:35:37 -0600

    HomePage Puff Counter

--- Models/DailyLog.cs
+++ Models/DailyLog.cs
@@ -8,7 +8,16 @@ namespace PuffPal.Models
         public int UserID { get; set; } // ID of the user this log belongs to
         public DateTime Date { get; set; } // The date of the log
         public int PuffsTaken { get; set; } // Number of puffs taken on this day
-        public string Mood { get; set; } // Mood of the user (e.g., "Happy", "Stressed")
-        public string Notes { get; set; } // Additional notes for the day
+        public string? Mood { get; set; } // Mood of the user (e.g., "Happy", "Stressed")
+        public string? Notes { get; set; } // Additional notes for the day
+
+        public DailyLog(int userId, DateTime date)
+        {
+            this.UserID = userId;
+            this.Date = date;
+            this.PuffsTaken = 0;
+            this.Mood = null;
+            this.Notes = null;
+        }
     }
 }
